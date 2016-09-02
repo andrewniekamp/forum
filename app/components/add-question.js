c95001f8d51df6aeac567f8ed566006ab24103c5
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-01 21:56:37 -0700

    add sendSave to question tile and update add question form

--- app/components/add-question.js
+++ app/components/add-question.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addQuestion', true);
+    }
+    sendSave() {
+      var params = {
+        text: this.get('text'),
+        notes: this.get('notes'),
+        author: this.get('author')
+      };
+      this.set('addQuestion', false);
+      this.sendAction('sendSave', params);
+      this.set('text', '');
+      this.set('notes', '');
+      this.set('author', '');
+    }
+  }
 });
