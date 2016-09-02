855922197f254121decc7819a9cd46f0bd6cd1d7
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-01 20:09:40 -0700

    adjust RSVP hash and display questions to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  //Model hook
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
+  }
 });
